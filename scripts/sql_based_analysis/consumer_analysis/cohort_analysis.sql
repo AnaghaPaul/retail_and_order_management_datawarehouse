b41ab1d93080023630303c0a49c83d9690cb44bb
Anagha Paul <152020111+AnaghaPaul@users.noreply.github.com>
2026-03-04 13:30:54 -0500

    Update cohort_analysis.sql

--- scripts/sql_based_analysis/consumer_analysis/cohort_analysis.sql
+++ scripts/sql_based_analysis/consumer_analysis/cohort_analysis.sql
@@ -1,4 +1,4 @@
--- __________________________________Cohort Analysis_________________________________________
+-- ___________________________________________________________Cohort Analysis___________________________________________________________________________________
 /* A Cohort Analysis compares similar groups over time.*/
 -- Type of cohort analysis - Returnship - Repeat Purchase Behaviour
 SELECT
@@ -16,10 +16,57 @@ ON f.shipping_date_key = s.shipping_date_key
 GROUP BY f.customer_key)a
 GROUP BY first_shipping_year;
 /*
-cohort_year	customers
-2011	      2178
-2012	      3220
+cohort_year	total_customers
+2014      	715
 2013	      12371
-2014	      715
+2012	      3220
+2011	      2178
 */
-
+WITH customer_cohort AS(
+SELECT
+f.customer_key,
+MIN(s.shipping_year) AS cohort_year,
+SUM(f.sales_amount) AS revenue_per_customer
+FROM
+gold.fact_sales AS f
+JOIN
+gold.dim_shipping_date AS s
+ON f.shipping_date_key=s.shipping_date_key
+GROUP BY f.customer_key)
+SELECT 
+customer_cohort.cohort_year,
+SUM(customer_cohort.revenue_per_customer) AS total_revenue_per_cohort
+FROM customer_cohort
+GROUP BY customer_cohort.cohort_year
+ORDER BY customer_cohort.cohort_year;
+/*
+cohort_year	total_revenue_per_cohort
+2011	      11289072
+2012	      11969678
+2013	      5973822
+2014	      123678
+*/
+WITH customer_cohort AS(
+SELECT
+f.customer_key,
+MIN(s.shipping_year) AS cohort_year,
+SUM(f.sales_amount) AS revenue_per_customer
+FROM
+gold.fact_sales AS f
+JOIN
+gold.dim_shipping_date AS s
+ON f.shipping_date_key=s.shipping_date_key
+GROUP BY f.customer_key)
+SELECT 
+customer_cohort.cohort_year,
+AVG( customer_cohort.revenue_per_customer) AS average_revenue_per_customer
+FROM customer_cohort
+GROUP BY customer_cohort.cohort_year
+ORDER BY customer_cohort.cohort_year;
+/*
+cohort_year	average_revenue_per_customer
+2011	       5183
+2012	       3717
+2013	       482
+2014	       172
+*/
